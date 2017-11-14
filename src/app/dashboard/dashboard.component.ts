6319fd39216d8848d11acbfc641d66f12e92cf6a
Simon Smith <simon@codesmith.dk>
2017-11-14 18:39:29 +0100

    First attempt at displaying list. Should probably use an observable rather than emptying the array every time there is an update.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -8,9 +13,29 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  listings: any[] = [];
+
+  constructor(
+    public afAuth: AngularFireAuth,
+    public afDb: AngularFireDatabase
+  ) {
+    this.afDb.object('Ideas')
+      .snapshotChanges()
+      .map(action =>{
+        const data = action.payload.toJSON();
+        return data;
+      }).subscribe(result =>{ 
+        this.listings = [];
+        Object.keys(result).map(key=>{ 
+          this.listings.push({ 
+            'key': key, 
+            'data':result[key] 
+          }); 
+        }); 
+        this.listings.reverse();
+      });
+   }
 
   ngOnInit() {
   }
-
 }
