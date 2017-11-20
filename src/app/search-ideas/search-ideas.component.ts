6fb73f22537df4c3ec8a808b73d0d73dec516a07
Lucku <lucku@web.de>
2017-11-20 16:43:05 +0100

    Some early additions in search component

--- src/app/search-ideas/search-ideas.component.ts
+++ src/app/search-ideas/search-ideas.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {UserService} from "../services/user.service";
+import {ActivatedRoute} from "@angular/router";
+import {Observable} from "rxjs/Observable";
+import {Idea} from "../Idea";
 
 @Component({
   selector: 'app-search-ideas',
@@ -8,9 +12,29 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class SearchIdeasComponent implements OnInit {
 
-  constructor() { }
+  isApproved: boolean;
+  searchTerm: string;
+
+  searchResults: Observable<Idea[]>;
+
+  constructor(private route: ActivatedRoute, private userService: UserService) {
+
+  }
 
   ngOnInit() {
+    this.userService.currentUser.subscribe((user) => {
+      if (user !== null) {
+        this.isApproved = user.Approved;
+      }
+      else {
+        this.isApproved = false;
+      }
+    });
+
+    if(this.route.snapshot.paramMap.get("term"))
+    {
+      this.searchTerm = this.route.snapshot.paramMap.get("term");
+    }
   }
 
 }
