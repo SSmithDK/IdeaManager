62c932926a9492bfd8409762118eeeb82db5b0e8
Simon Smith <simon@codesmith.dk>
2017-11-24 17:17:14 +0100

    Example for testing HTML elements

--- src/app/idea/idea.component.spec.ts
+++ src/app/idea/idea.component.spec.ts
@@ -8,10 +8,15 @@ import { UserService } from '../services/user.service';
 import { MockUserService } from '../mockservices/mock-user.service';
 import { IdeaService } from '../services/idea.service';
 import { MockIdeaService } from '../mockservices/mock-idea.service';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { Idea } from '../Idea';
 
 describe('IdeaComponent', () => {
   let component: IdeaComponent;
   let fixture: ComponentFixture<IdeaComponent>;
+  let titleElement: HTMLElement;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -29,9 +34,24 @@ describe('IdeaComponent', () => {
     fixture = TestBed.createComponent(IdeaComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    let testIdea = new Idea;
+    testIdea.title = "Test idea";
+    testIdea.tags = [];
+    component.idea = testIdea;
+
+
+    fixture.detectChanges(); // trigger initial data binding
+    debugElement = fixture.debugElement.query(By.css('h4.idea-title'));
+    titleElement = debugElement.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should have idea title', () => {
+    fixture.detectChanges();
+    expect(titleElement.textContent).toBe(component.idea.title);
   });
 });
