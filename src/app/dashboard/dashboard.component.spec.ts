9f291bc44afb7daa42193168297f32d45ceed08e
Simon Smith <simon@codesmith.dk>
2017-12-01 11:58:10 +0100

    Adding test cases

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -11,10 +11,14 @@ import {UserService} from "../services/user.service";
 import { AddingReferenceIdeaComponent } from '../adding-reference-idea/adding-reference-idea.component';
 import { SplitAndGetLastPipe } from '../pipes/split-and-get-last.pipe';
 import {TextFormattingPipe} from "../pipes/text-formatting.pipe";
+import { DebugElement } from '@angular/core/src/debug/debug_node';
+import { By } from '@angular/platform-browser';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let htmlElement: HTMLElement;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -36,5 +40,43 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should have grid button', () => {
+    debugElement = fixture.debugElement.query(By.css('#grid'));
+    expect(debugElement.nativeElement).toBeTruthy();
+  });
+
+  it('Should have list button', () => {
+    debugElement = fixture.debugElement.query(By.css('#list'));
+    expect(debugElement.nativeElement).toBeTruthy();
+  });
+
+  it('List should be default', () => {
+    debugElement = fixture.debugElement.query(By.css('#list.active'));
+    expect(debugElement.nativeElement).toBeTruthy();
+    debugElement = fixture.debugElement.query(By.css("#ideas.list-group"));
+    expect(debugElement.nativeElement).toBeTruthy();
+  });
+
+  it('Should change between list and grid', () => {
+    let button = fixture.debugElement.query(By.css('#grid'));
+    button.nativeElement.click();
+
+    // Switch to grid
+    fixture.whenStable().then(() => {
+      debugElement = fixture.debugElement.query(By.css("#ideas .item .grid-group-item"))[0];
+      expect(debugElement.nativeElement).toBeTruthy();
+    });
+
+    button = fixture.debugElement.query(By.css('#list'));
+    button.nativeElement.click();
+
+    // Switch back to list
+    fixture.whenStable().then(() => {
+      debugElement = fixture.debugElement.query(By.css("#ideas .item .list-group-item-light"))[0];
+      expect(debugElement.nativeElement).toBeTruthy();
+    });
+
   });
 });
